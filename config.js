4c8c61bae5a4b63f5c5284c2a1a1ea2e08f3fe82
saviya2009 <126062795+saviya2009@users.noreply.github.com>
2023-10-18 19:51:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ function convertToBool(text, fault = 'true') {
 
 // PUT YOUR SESSION ID HERE  ##Required 
 
-global.SESSION_ID = 'QueenNilu;;;52hSzSJC#zMCiE_tJLt1ws4eEYrmXkHpe6d-tzKqJOyMZ-5P6YlI' // PUT your session ID BY scan QR 
+global.SESSION_ID = 'QueenNilu;;;ovxQ3Aab#L_nL92ACgys_xcOBohk-XBpFYo_Ea7qhLOszFeNfWTQ' // PUT your session ID BY scan QR 
 
 // databse URL  ##Required 
 global.POSTGRESQL_URL = 'postgres://nilu_md_user:HKW7EHkDcOkd95t38g0crNZfzfYhjMRV@dpg-cknuchea02is73c0aur0-a.frankfurt-postgres.render.com/nilu_md'
